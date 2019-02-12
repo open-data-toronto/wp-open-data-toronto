19680cf82f0fb13064bc59d5df94aeb2e7a4d7de
Yizhao <yizhao.tan@toronto.ca>
2019-02-12 10:50:38 -0500

    Hides search msg by default

--- wp-open-data-toronto/page-catalogue.php
+++ wp-open-data-toronto/page-catalogue.php
@@ -92,7 +92,7 @@
                 </span>
               </div>
               </form>
-              <small id="search-error" class="text-danger"><strong>Only numbers, letters, and spaces are allowed</strong></small>
+              <small id="search-error" class="text-danger hidden"><strong>Only numbers, letters, and spaces are allowed</strong></small>
             </div>
             <div class="row" id="dataset-search-results">
               <div class="col-md-8 info count">
