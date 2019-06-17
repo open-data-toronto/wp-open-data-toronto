aef806f7ddb5ea4c3bd4b4d7e1b59e02bc646a18
sidramatik <sidra@oneseventyfive.com>
2019-06-17 10:39:22 -0400

    Added new logo images and updated header and footer specs as per strat comm guidelines

--- wp-open-data-toronto/footer.php
+++ wp-open-data-toronto/footer.php
@@ -6,7 +6,7 @@
       <p>
         <a href="https://www.toronto.ca/home/copyright-information/">
           <i aria-hidden="true" class="fa fa-copyright"></i>
-          <span class="sr-only">Copyright</span> City of Toronto 1998 - 2018
+          <span class="sr-only">Copyright</span> City of Toronto 1998 - 2019
         </a>
       </p>
     </div>
