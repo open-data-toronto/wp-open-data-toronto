9fd662f1a809f0d1eb5004accc6731e438227735
dottyz <yizhao.tan@gmail.com>
2019-07-04 14:30:11 -0400

    URL linking

--- wp-open-data-toronto/js/config.js
+++ wp-open-data-toronto/js/config.js
@@ -1,13 +1,9 @@
 if (window.location.host.indexOf('intra') !== -1) {
-    var envName = window.location.host.split('.').length >= 4 ? window.location.host.split('.')[2].split('-')[0] : 'dev',
-        envNum = (window.location.host.match(/\d/g) != null) ? parseInt(window.location.host.match(/\d/g).join('')) : 1,
-        ckan = 'ckanadmin' + envNum + '.intra.' + envName + '-toronto.ca';
-} else {
+    var ckan = window.location.host.replace('odadmin', 'ckanadmin');
+} else if (window.location.host.indexOf('inter') !== -1) {
     var ckan = window.location.host.replace('portal', 'ckan');
-}
-
-if (ckan.indexOf('toronto.ca') == -1) {
-    ckan = 'http://localhost:5000';
+} else {
+    var ckan = 'ckan.cf.opendata.inter.prod-toronto.ca'
 }
 
 var config = {
