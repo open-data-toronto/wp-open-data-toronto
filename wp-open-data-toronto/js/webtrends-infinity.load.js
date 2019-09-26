2e7a99d15418a48220e7e25e904f776ae398ba80
dottyz <yizhao.tan@gmail.com>
2019-09-26 13:58:15 -0400

    Fix typo

--- wp-open-data-toronto/js/webtrends-infinity.load.js
+++ wp-open-data-toronto/js/webtrends-infinity.load.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function() {
      // add meta tags
      $("head").append("<meta name='DCSext.dcsid' content='dcs222ldvxk938tpne9uk1e3u_1c4g'/>");
-   }
       scriptName = "https://c.oracleinfinity.io/acs/account/97j62divdr/js/inpage/odc.js";
    $.getScript(scriptName, function() {
    });
