26f0991927525f269aa76cd3890b460485085292
vishbhav25 <Vishwa.Bhavsar@Toronto.ca>
2019-10-28 15:43:57 -0400

    Create the pre footer for the news letter subscription

--- wp-open-data-toronto/footer.php
+++ wp-open-data-toronto/footer.php
@@ -1,6 +1,31 @@
 </main>
 <?php wp_footer(); ?>
 
+<footer id="pre-footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-6">
+          <a href="mailto:opendata@toronto.ca">
+            <i class="fa fa-envelope"> </i> 
+          </a>
+          <a href="https://twitter.com/Open_TO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </div>     
+      <!-- Begin Mailchimp Signup Form -->
+        <div id="col-sm-9 col-md-6">
+          <form action="https://toronto.us7.list-manage.com/subscribe/post?u=4bfad28974017cb009fd6d24a&amp;id=36d03c2b81" method="post" name="mc-embedded-subscribe-form"  target="_blank" novalidate>
+	          <label for="monthly updates" id="monthly-updates"><b>Receive monthly updates</b></label>
+	            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+              <input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe"  class="btn btn-primary my-2 my-sm-0">
+          </form>
+          <!--End mc_embed_signup-->
+        </div>
+      </div>
+  </div>
+</footer>
+
+
   <footer id="footer" role="contentinfo">
     <div class="container">
       <p>
