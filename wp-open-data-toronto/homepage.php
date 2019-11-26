77960ee8d7c4348c18e45689d5ac981ad1d4889f
dottyz <yizhao.tan@gmail.com>
2019-11-26 11:28:56 -0500

    Update homepage ul element class

--- wp-open-data-toronto/homepage.php
+++ wp-open-data-toronto/homepage.php
@@ -50,7 +50,7 @@
 
             <div class="col-md-6 offset-md-1">
               <h2>Recently Added Datasets</h2>
-              <div class="newsfeed"></div>
+              <div class="newsfeed row"></div>
               <a class="btn btn-md" href="/catalogue/">Explore the Catalogue</a>
             </div>
           </div>
